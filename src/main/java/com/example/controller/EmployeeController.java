f927c79522f68e04d7ad9e33ccd3072577749970
Sota-Akahane <sota258@docomo.ne.jp>
2025-05-21 14:35:18 +0900

    扶養人数更新における入力値チェックを追加した。

--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -63,7 +65,14 @@ public class EmployeeController {
      * @return 従業員リストを出力する画面にフォワード
      */
     @PostMapping("/update")
-    public String update(UpdateEmployeeForm form) {
+    public String update(@Validated UpdateEmployeeForm form,
+                         BindingResult result,
+                         Model model) {
+
+        if (result.hasErrors()) {
+            return showDetail(form.getId(), model, form);
+        }
+
         Employee employee
                 = employeeService.showDetail(form.getIntId());
         employee.setDependentsCount(form.getIntDependentsCount());
--- src/main/java/com/example/form/UpdateEmployeeForm.java
+++ src/main/java/com/example/form/UpdateEmployeeForm.java
@@ -1,5 +1,7 @@
 package com.example.form;
 
+import jakarta.validation.constraints.Pattern;
+
 /**
  * 従業員更新時に使用するフォーム.
  *
@@ -9,6 +11,7 @@ public class UpdateEmployeeForm {
     /** 従業員ID */
     private String id;
     /** 扶養人数 */
+    @Pattern(regexp = "^\\d+$", message = "数値を入力してください。")
     private String dependentsCount;
 
     public Integer getIntId() {
