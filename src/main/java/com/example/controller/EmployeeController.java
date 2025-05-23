048ba46db57afda2f0e65d19eaf2179e7201a411
Sota-Akahane <sota258@docomo.ne.jp>
2025-05-23 14:13:32 +0900

    ログインしていなければサイト閲覧ができないようにした。

--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -3,6 +3,8 @@ package com.example.controller;
 import com.example.domain.Employee;
 import com.example.form.UpdateEmployeeForm;
 import com.example.service.EmployeeService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
@@ -12,6 +14,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -26,14 +29,24 @@ public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
+    @Autowired
+    private HttpSession session;
+
     /**
      * 従業員一覧を出力する.
+     * ログインしていなければ閲覧できない。
      *
      * @param model リクエストスコープ
      * @return 従業員一覧画面にフォワード
      */
     @GetMapping("/showList")
     public String showList(Model model) {
+        // 存在するURLのヒントを与えないために、エラーメッセージ無しで単にログイン画面に戻す。
+        // URLをトップに戻すために、フォワードではなくリダイレクトにする。
+//        if (session.getAttribute("administratorName") == null) {
+//            return "redirect:/";
+//        }
+
         List<Employee> employeeList
                 = employeeService.showList();
         model.addAttribute("employeeList", employeeList);
@@ -51,9 +64,20 @@ public class EmployeeController {
      */
     @GetMapping("/showDetail")
     public String showDetail(String id, Model model, UpdateEmployeeForm form) {
+//        if (session.getAttribute("administratorName") == null) {
+//            return "redirect:/";
+//        }
+
         int intId = Integer.parseInt(id);
         Employee employee = employeeService.showDetail(intId);
+
+        BeanUtils.copyProperties(employee, form);
+        form.setId(id);
+        form.setSalary(Integer.toString(employee.getSalary()));
+        form.setDependentsCount(Integer.toString(employee.getDependentsCount()));
+
         model.addAttribute("employee", employee);
+        model.addAttribute("updateEmployeeForm", form);
 
         return "employee/detail";
     }
@@ -70,11 +94,14 @@ public class EmployeeController {
                          Model model) {
 
         if (result.hasErrors()) {
+            System.out.println("koko");
             return showDetail(form.getId(), model, form);
         }
 
         Employee employee
                 = employeeService.showDetail(form.getIntId());
+        employee.setName(form.getName());
+        employee.setSalary(form.getIntSalary());
         employee.setDependentsCount(form.getIntDependentsCount());
         employeeService.update(employee);
 
--- src/main/java/com/example/form/UpdateEmployeeForm.java
+++ src/main/java/com/example/form/UpdateEmployeeForm.java
@@ -1,6 +1,9 @@
 package com.example.form;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+
+import java.util.Date;
 
 /**
  * 従業員更新時に使用するフォーム.
@@ -10,12 +13,44 @@ import jakarta.validation.constraints.Pattern;
 public class UpdateEmployeeForm {
     /** 従業員ID */
     private String id;
+    /** 名前 */
+    @NotBlank(message = "名前を入力してください。")
+    private String name;
+    /** 画像 */
+    private String image;
+    /** 性別 */
+//    @NotBlank(message = "性別を入力してください。")
+    private String gender;
+    /** 入社日 */
+//    @NotBlank(message = "入社日を入力してください。")
+    private Date hireDate;
+    /** メールアドレス */
+//    @NotBlank(message = "メールアドレスを入力してください。")
+    private String mailAddress;
+    /** 郵便番号 */
+//    @NotBlank(message = "郵便番号を入力してください。")
+    private String zipCode;
+    /** 住所 */
+//    @NotBlank(message = "住所を入力してください。")
+    private String address;
+    /** 電話番号 */
+//    @NotBlank(message = "電話番号を入力してください。")
+    private String telephone;
+    /** 給料 */
+    @NotBlank(message = "給料を入力してください。")
+    private String  salary;
+    /** 特性 */
+    private String characteristics;
     /** 扶養人数 */
     @Pattern(regexp = "^\\d+$", message = "数値を入力してください。")
     private String dependentsCount;
 
     public Integer getIntId() {
         return Integer.parseInt(id);
+    }
+
+    public Integer getIntSalary() {
+        return Integer.parseInt(salary);
     }
 
     public Integer getIntDependentsCount() {
@@ -30,6 +65,86 @@ public class UpdateEmployeeForm {
         this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(Date hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public String getMailAddress() {
+        return mailAddress;
+    }
+
+    public void setMailAddress(String mailAddress) {
+        this.mailAddress = mailAddress;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+
+    public String getCharacteristics() {
+        return characteristics;
+    }
+
+    public void setCharacteristics(String characteristics) {
+        this.characteristics = characteristics;
+    }
+
     public String getDependentsCount() {
         return dependentsCount;
     }
@@ -42,6 +157,16 @@ public class UpdateEmployeeForm {
     public String toString() {
         return "UpdateEmployeeForm{" +
                 "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", image='" + image + '\'' +
+                ", gender='" + gender + '\'' +
+                ", hireDate=" + hireDate +
+                ", mailAddress='" + mailAddress + '\'' +
+                ", zipCode='" + zipCode + '\'' +
+                ", address='" + address + '\'' +
+                ", telephone='" + telephone + '\'' +
+                ", salary='" + salary + '\'' +
+                ", characteristics='" + characteristics + '\'' +
                 ", dependentsCount='" + dependentsCount + '\'' +
                 '}';
     }
